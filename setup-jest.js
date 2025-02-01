1d0e6c464349654e299639458e422b5a58de0118
Mary Zin <zin.marie.v@gmail.com>
2025-02-01 20:11:35 +0300

    feat: setting jest

---
+++ setup-jest.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+global.$ = $;
+
+// // If you want to mock bootstrap
+// global.$.fn.modal = jest.fn(() => $());
+// global.$.fn.carousel = jest.fn(() => $());
+// global.$.fn.tooltip = jest.fn(() => $());
