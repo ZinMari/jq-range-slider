a84d09f728ae078697b785348d53bb7fbfd78673
Mary Zin <zin.marie.v@gmail.com>
2025-02-01 20:12:32 +0300

    feat: create tests for sliderLineView class

---
+++ src/tests/View/SubViews/sliderLineView.test.ts
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom';
+import { screen, queryHelpers } from '@testing-library/dom';
+import SliderLineView from '../../../View/SliderLineView';
+
+describe('Slider line class', () => {
+  let wrappElement: JQuery<HTMLElement>;
+  let lineElement: BaseSubViewInterface;
+  let className: string;
+  const baseClassName: string = 'alexandr__line';
+
+  beforeEach(() => {
+    wrappElement = $('<div>');
+    className = Math.random().toString();
+    lineElement = new SliderLineView(wrappElement, className);
+  });
+
+  test(`Элемент содержит произвольный класс назначенный пользователем, например ${className}`, () => {
+    expect(lineElement.item[0]).toHaveClass(className);
+  });
+
+  test(`Элемент содержит базовый класс`, () => {
+    expect(lineElement.item[0]).toHaveClass(baseClassName);
+  });
+
+  test(`Элемент линии был добавлен в родителя`, () => {
+    expect(wrappElement[0]).toContainElement(lineElement.item[0]);
+  });
+});
