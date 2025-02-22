3072d5cef1258daa3f098ca0401ffe7c3168787b
Mary Zin <zin.marie.v@gmail.com>
2025-02-22 05:36:46 +0300

    feat: create option method

--- src/alexandr/jquery.alexandr.ts
+++ src/alexandr/jquery.alexandr.ts
@@ -13,7 +13,63 @@ import './jquery.alexandr.scss';
     }
   }
 
-  const defaults: AlexandrSettings = {
+  $.extend(Alexandr.prototype, {
+    _optionPlugin(target: any, options: any, value: any) {
+      target = $(target);
+
+      const inst = target.data('alexandrOptions');
+
+      if (!options || (typeof options == 'string' && value == null)) {
+        var name = options;
+        options = inst || {};
+        return options && name ? options[name] : options;
+      }
+
+      //   if (!target.hasClass(this.markerClassName)) {
+      //     return;
+      //   }
+
+      //   options = options || {};
+
+      //   if (typeof options === 'string') {
+      //     const name = options;
+      //     options = {};
+      //     options[name] = value;
+      //   }
+
+      //   $.extend(inst.options, options);
+    },
+  });
+
+  function isNotChained(method: any, otherArgs: any) {
+    if (
+      method === 'option' &&
+      (otherArgs.length === 0 || (otherArgs.length === 1 && typeof otherArgs[0] === 'string'))
+    ) {
+      return true;
+    }
+  }
+
+  $.fn.alexandr = function (options: any) {
+    const otherArgs = Array.prototype.slice.call(arguments, 1);
+
+    if (isNotChained(options, otherArgs)) {
+      const plugin = $(this).data('alexandr');
+      return plugin['_' + options + 'Plugin'].apply(plugin, [this[0]].concat(otherArgs));
+    }
+
+    const config = $.extend({}, $.fn.alexandr.defaults, options);
+    config.container = this;
+
+    return this.each(function () {
+      if (!$(this).data('alexandr')) {
+        $(this).data('alexandr', new Alexandr(config));
+        $(this).data('alexandrOptions', config);
+      }
+    });
+  };
+
+  $.fn.alexandr.defaults = {
     minValue: 0,
     maxValue: 100,
     stepValue: 10,
@@ -35,17 +91,11 @@ import './jquery.alexandr.scss';
     showMinValueClass: '',
     showMaxValueClass: '',
   };
-
-  $.fn.alexandr = function (options: any) {
-    const config = $.extend({}, defaults, options);
-    config.container = this;
-
-    return this.each(function () {
-      if (!$(this).data('alexandr')) {
-        $(this).data('alexandr', new Alexandr(config));
-      }
-    });
-  };
 })(jQuery);
 
+$.fn.alexandr.defaults.minValue = 10;
+
 $('.container').alexandr();
+console.log($('.container').alexandr('option'));
+
+$('.container1').alexandr({ orientation: 'horizontal', type: 'single' });
