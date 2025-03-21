c1ee190552c1df9b3f0decaf3c6d43392d9c2c24
Mari Zin <mari.v.zinkevich@gmail.com>
2025-03-21 21:21:34 +0300

    refactor: lint buildPlaguins.ts

--- config/build/buildPlaguins.ts
+++ config/build/buildPlaguins.ts
@@ -1,14 +1,17 @@
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
-import { BuildOptions } from './types/types';
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import FaviconsWebpackPlugin from "favicons-webpack-plugin";
+import { BuildOptions } from "./types/types";
 
-export function buildPlaguins({ mode, paths }: BuildOptions): webpack.Configuration['plugins'] {
-  const isDev = mode === 'development';
-  const isProd = mode === 'production';
-  const plugins: webpack.Configuration['plugins'] = [
-    new HtmlWebpackPlugin({ 
+export function buildPlaguins({
+  mode,
+  paths,
+}: BuildOptions): webpack.Configuration["plugins"] {
+  const isDev = mode === "development";
+  const isProd = mode === "production";
+  const plugins: webpack.Configuration["plugins"] = [
+    new HtmlWebpackPlugin({
       template: paths.html,
     }),
   ];
@@ -16,32 +19,32 @@ export function buildPlaguins({ mode, paths }: BuildOptions): webpack.Configurat
   if (isDev) {
     plugins.push(
       new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery',
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
       }),
     );
   }
   if (isProd) {
     plugins.push(
       new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
+        filename: "css/[name].[contenthash:8].css",
+        chunkFilename: "css/[name].[contenthash:8].css",
       }),
-      new FaviconsWebpackPlugin({
-        logo: './src/page/favicon.png',
-        mode: 'webapp',
-        devMode: 'webapp',
-        prefix: 'assets/favicons/',
-        cache: true,
-        inject: htmlPlugin => {
-          return true
-        },
-        favicons: {
-          background: '#fff',
-          theme_color: '#333'
-        }
-      }),
+      // new FaviconsWebpackPlugin({
+      //   logo: './src/page/favicon.png',
+      //   mode: 'webapp',
+      //   devMode: 'webapp',
+      //   prefix: 'assets/favicons/',
+      //   cache: true,
+      //   inject: htmlPlugin => {
+      //     return true
+      //   },
+      //   favicons: {
+      //     background: '#fff',
+      //     theme_color: '#333'
+      //   }
+      // }),
     );
   }
   return plugins;
