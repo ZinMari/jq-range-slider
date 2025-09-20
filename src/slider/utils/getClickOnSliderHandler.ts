21edc761df25ef0839c767f8db746f94c3a7b30c
ZinMari <mari.v.zinkevich@gmail.com>
2025-09-20 09:07:42 +0300

    reafactor: #15 rename handlerClickOnSlider utils to getClickOnSliderHandler

---
+++ src/slider/utils/getClickOnSliderHandler.ts
@@ -0,0 +1,36 @@
+import getCoordinates from "./getCoordinates";
+
+const getClickOnSliderHandler = (
+  event: PointerEvent,
+  line: JQuery<HTMLElement>,
+  orientation: "horizontal" | "vertical",
+  notify: (
+    eventName: "clickOnSlider",
+    data: {
+      pixelClick: number;
+    },
+  ) => void,
+) => {
+  event.preventDefault();
+  const target = event.currentTarget;
+
+  if (target instanceof HTMLElement) {
+    if (target.classList.contains("slider__thumb")) {
+      return;
+    }
+  }
+
+  const sliderLineCoordinates = getCoordinates(line);
+
+  // на скольких пикселях от линии произошел клик
+  const pixelClick =
+    orientation === "horizontal"
+      ? event.pageX - sliderLineCoordinates.left
+      : event.pageY - sliderLineCoordinates.top;
+
+  notify("clickOnSlider", {
+    pixelClick,
+  });
+};
+
+export default getClickOnSliderHandler;
