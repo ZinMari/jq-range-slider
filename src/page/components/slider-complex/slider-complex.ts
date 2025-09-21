b561283f1c4a2b6eca991bbf8067d264df4a81bc
M <mari.v.zinkevich@gmail.com>
2025-09-21 14:05:23 +0300

    refactor: #17 add strict to ts

--- src/page/components/slider-complex/slider-complex.ts
+++ src/page/components/slider-complex/slider-complex.ts
@@ -21,7 +21,7 @@ function setValueToPanel(slider: string): void {
   const $number: JQuery<HTMLElement> = $panel.find("input[type=number]");
 
   $.each($radio, function () {
-    const attrName: string = $(this).attr("name");
+    const attrName: string | undefined = $(this).attr("name");
     if (
       $(this).attr("value") === sliderOptions[attrName as keyof TSliderSettings]
     ) {
@@ -30,15 +30,17 @@ function setValueToPanel(slider: string): void {
   });
 
   $.each($number, function () {
-    const attrName: string = $(this).attr("name");
-    $(this).attr(
-      "value",
-      sliderOptions[attrName as keyof TSliderSettings].toString(),
-    );
+    const attrName: string | undefined = $(this).attr("name");
+    if (attrName && sliderOptions) {
+      const value = sliderOptions[attrName as keyof TSliderSettings];
+      if (value !== undefined) {
+        $(this).attr("value", value.toString());
+      }
+    }
   });
 
   $.each($checkbox, function () {
-    const attrName: string = $(this).attr("name");
+    const attrName: string | undefined = $(this).attr("name");
     $(this).prop("checked", sliderOptions[attrName as keyof TSliderSettings]);
   });
 }
@@ -46,33 +48,42 @@ function setValueToPanel(slider: string): void {
 function onChangePanelValue(event: Event) {
   event.preventDefault();
 
-  const $target: JQuery<EventTarget> = $(event.target);
-  const attrType = $target.attr("type");
+  const targetElement = event.target as HTMLElement | null;
 
-  if (attrType === "radio" || attrType === "number") {
-    $target
-      .closest(".js-slider-complex")
-      .find(".js-slider-complex__slider")
-      .slider("update", { [$target.attr("name")]: $target.val() });
-  }
+  if (targetElement) {
+    const $target: JQuery<EventTarget> = $(targetElement);
+    const attrType = $target.attr("type");
+    const nameAttr = $target.attr("name");
 
-  if (attrType === "checkbox") {
-    $target
-      .closest(".js-slider-complex")
-      .find(".js-slider-complex__slider")
-      .slider("update", { [$target.attr("name")]: $target.prop("checked") });
+    if (nameAttr) {
+      if (attrType === "radio" || attrType === "number") {
+        $target
+          .closest(".js-slider-complex")
+          .find(".js-slider-complex__slider")
+          .slider("update", { [nameAttr]: $target.val() });
+      }
+
+      if (attrType === "checkbox") {
+        $target
+          .closest(".js-slider-complex")
+          .find(".js-slider-complex__slider")
+          .slider("update", { [nameAttr]: $target.prop("checked") });
+      }
+    }
   }
 }
 
 function onSliderValueChange(options: TSliderSettings) {
-  const $panel = options.container.parent().find(".js-slider-complex__panel");
-  $panel.find(".js-form__controlMinThumb").val(options.minPosition);
-  $panel.find(".js-form__controlMaxThumb").val(options.maxPosition);
-  $panel.find(".js-form__controlMinValue").val(options.minValue);
-  $panel.find(".js-form__controlMaxValue").val(options.maxValue);
-  $panel.find(".js-form__controlStep").val(options.stepValue);
-  $panel.find(".js-form__controlFlag").prop("checked", options.showValueFlag);
-  $panel.find(".js-form__controlRuler").prop("checked", options.showRuler);
+  const $panel = options.container?.parent().find(".js-slider-complex__panel");
+  if ($panel) {
+    $panel.find(".js-form__controlMinThumb").val(options.minPosition ?? 0);
+    $panel.find(".js-form__controlMaxThumb").val(options.maxPosition ?? 0);
+    $panel.find(".js-form__controlMinValue").val(options.minValue ?? 0);
+    $panel.find(".js-form__controlMaxValue").val(options.maxValue ?? 0);
+    $panel.find(".js-form__controlStep").val(options.stepValue ?? 0);
+    $panel.find(".js-form__controlFlag").prop("checked", options.showValueFlag);
+    $panel.find(".js-form__controlRuler").prop("checked", options.showRuler);
+  }
 }
 
 function initSliderComplex(slider: string, options: TSliderSettings) {
