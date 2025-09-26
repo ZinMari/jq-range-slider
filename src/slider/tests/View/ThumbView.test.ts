53a13559d6319909e9711c40d40c2d16d64e80a5
M <mari.v.zinkevich@gmail.com>
2025-09-26 10:59:32 +0300

    refactor: #17 fix strict in ThumbView.test.ts

--- src/slider/tests/View/ThumbView.test.ts
+++ src/slider/tests/View/ThumbView.test.ts
@@ -1,9 +1,6 @@
-// @ts-nocheck
 import "@testing-library/jest-dom";
-import { fireEvent } from "@testing-library/dom";
 import ThumbView from "../../View/ThumbView/ThumbView";
 import LineView from "../../View/LineView/LineView";
-import getRandomInteger from "../../utils/getRandomInteger";
 
 describe("Кнопки:", () => {
   const thumbs = new ThumbView({
@@ -17,20 +14,24 @@ describe("Кнопки:", () => {
   thumbs.updateFlagValues("max", 100);
 
   test("Обновляют значения флажков", () => {
-    const oldMaxValueFlag = thumbs.maxThumb.attr("data-value");
+    const oldMaxValueFlag = thumbs.maxThumb?.attr("data-value");
     const oldMinValueFlag = thumbs.minThumb.attr("data-value");
     thumbs.updateFlagValues("max", 300);
     thumbs.updateFlagValues("min", 600);
-    expect(oldMaxValueFlag).not.toBe(thumbs.maxThumb.attr("data-value"));
+    expect(oldMaxValueFlag).not.toBe(thumbs.maxThumb?.attr("data-value"));
     expect(oldMinValueFlag).not.toBe(thumbs.minThumb.attr("data-value"));
   });
   test("Меняют ориентацию", () => {
     thumbs.setOrientation("vertical");
-    expect(thumbs.maxThumb[0]).toHaveClass("slider__thumb_type_vertical");
+    if (thumbs.maxThumb) {
+      expect(thumbs.maxThumb[0]).toHaveClass("slider__thumb_type_vertical");
+    }
   });
   test("Скрывают флажки", () => {
     thumbs.hideFlag();
-    expect(thumbs.maxThumb[0]).not.toHaveClass("flag");
+    if (thumbs.maxThumb) {
+      expect(thumbs.maxThumb[0]).not.toHaveClass("flag");
+    }
   });
   test("Показывают флажки", () => {
     thumbs.showFlag();
@@ -38,6 +39,8 @@ describe("Кнопки:", () => {
   });
   test("Обновляют позицию", () => {
     thumbs.updateThumbsPosition("max", 100, "left");
-    expect(thumbs.maxThumb[0]).toHaveStyle({ left: "100px" });
+    if (thumbs.maxThumb) {
+      expect(thumbs.maxThumb[0]).toHaveStyle({ left: "100px" });
+    }
   });
 });
